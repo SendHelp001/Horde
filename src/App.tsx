3ad4bfdd86265cc695c3970a24f177bdf2c9b87c
SendHelp001 <jonardsalanip@gmail.com>
2025-05-05 14:10:04 +0800

    refactor: Organize imports in App.tsx for better readability and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-// App.tsx
 import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact,
+} from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Menu from "./pages/Menu";
-import { AuthProvider } from "./utils/AuthProvider";
+import {
+  homeOutline,
+  addOutline,
+  searchOutline,
+  personOutline,
+  notificationsOutline,
+} from "ionicons/icons";
 
+
+/* Core & optional CSS */
 import "@ionic/react/css/core.css";
 import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
 import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Dark mode system preference */
 import "@ionic/react/css/palettes/dark.system.css";
+
+/* Theme variables */
 import "./theme/variables.css";
+import { AuthProvider } from "./utils/AuthProvider";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
+import Signup from "./pages/Signup";
 
 setupIonicReact();
 
