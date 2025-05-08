431bc1305fcb70efca4d47f1749dd58dab16ad72
SendHelp001 <jonardsalanip@gmail.com>
2025-05-08 21:24:16 +0800

    Updates

--- assets/md.transition-legacy-CXkItsH8.js
+++ assets/md.transition-legacy-CXkItsH8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CxY-LdBg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DlLpb4wv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
