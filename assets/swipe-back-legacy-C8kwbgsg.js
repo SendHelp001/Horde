3b516b2d39ae036a7c15f1cffc5c9da5131bb883
SendHelp001 <jonardsalanip@gmail.com>
2025-05-08 21:54:22 +0800

    Updates

--- assets/swipe-back-legacy-C8kwbgsg.js
+++ assets/swipe-back-legacy-C8kwbgsg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CN1LtmLh.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Dl3VoqeH.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
